7e44c170aeed8b94c5b3e3bb4278c4111a458347
Ali-95705Al <120209968+Ali-95705Al@users.noreply.github.com>
2023-01-09 21:29:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result;
+result = a - b;
+
+//Если result < 0 => a меньше b
+//Если result > 0 => a больше b
+
+if (result < 0)
+{      //Если result< 0 =>
+   Console.WriteLine ("a меньше b");
+}
+else
+{    // Если result > 0 =>
+   Console.WriteLine ("a больше b"); 
+}
